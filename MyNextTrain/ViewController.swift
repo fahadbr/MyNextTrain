46420b31cf207bd7c8f7aafaccbd6acc7a5ef9f8
fahadr <fahadbr@gmail.com>
2016-08-13 22:38:12 -0400

    Initial Commit

---
+++ MyNextTrain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNextTrain
+//
+//  Created by FAHAD RIAZ on 8/13/16.
+//  Copyright © 2016 FAHAD RIAZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
