81a08c4a9b6ac57d3cbff5d7efd19b656469799d
fahadr <fahadbr@gmail.com>
2016-09-11 00:12:00 -0400

    unstable temp

---
+++ MyNextTrain/AttributedStringBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  AttributedStringBuilder.swift
+//  MyNextTrain
+//
+//  Created by FAHAD RIAZ on 9/10/16.
+//  Copyright © 2016 FAHAD RIAZ. All rights reserved.
+//
+
+import Foundation
--- MyNextTrain/RealmQueryService.swift
+++ MyNextTrain/RealmQueryService.swift
@@ -39,12 +39,26 @@ class RealmQueryService: QueryService {
 		}
 		return []
 	}
+    
+    private lazy var routesById: [Int : RouteImpl] = self.loadRoutesById()
 	
+    private func loadRoutesById() -> [Int : RouteImpl] {
+        do {
+            let realm = try Realm()
+            let routes = realm.allObjects(ofType: RouteImpl.self)
+            Logger.debug("loaded \(routes.count) routes")
+            return routes.dictionary(keyExtractor: { $0.id })
+        
+        } catch let e {
+            Logger.error("error fetching routes", error: e)
+        }
+        return [:]
+    }
 	
 	func tripSummaries(for pairing: StopPairing, on date: Date) -> [TripSummary] {
 		guard let startingStop = pairing.startingStop, let destinationStop = pairing.destinationStop else {
-				Logger.error("one of startingStop or destinationStop was nil in pairing")
-				return []
+            Logger.error("one of startingStop or destinationStop was nil in pairing")
+            return []
 		}
 		
 		do {
@@ -71,6 +85,7 @@ class RealmQueryService: QueryService {
                     summary._startingStop = startingStopTime
                     summary._destinationStop = destinationStopTime
                     summary._trip = trip
+                    summary._route = routesById[trip.routeId]
                     summaries.append(summary)
                 }
             }
@@ -85,66 +100,78 @@ class RealmQueryService: QueryService {
 	}
 	
 	func nextTripSummary(forPairing pairing: StopPairing) -> TripSummary? {
-		guard let startingStop = pairing.startingStop, let destinationStop = pairing.destinationStop else {
-			Logger.error("one of startingStop or destinationStop was nil in pairing")
-			return nil
-		}
-		
-		do {
-			let realm = try Realm()
-			Logger.debug("searching for next trip from \(startingStop.name) to \(destinationStop.name)")
-			
-			let date = currentDate
-			let currentTimeInterval = Date().timeIntervalSince(date)
-			
-			let startingStopTimes = realm.allObjects(ofType: StopTimeImpl.self)
-				.filter(using: "stopId == %@ AND departureTime >= %@", startingStop.id, currentTimeInterval)
-				.sorted(onProperty: "departureTime")
-			
-			let destinationStopTimes = realm.allObjects(ofType: StopTimeImpl.self)
-				.filter(using: "stopId == %@ AND arrivalTime >= %@", destinationStop.id, currentTimeInterval)
-				.sorted(onProperty: "arrivalTime")
-			
-			
-			let commonTripIds = Set<String>(startingStopTimes.map { $0.tripId })
-				.intersection(Set<String>(destinationStopTimes.map { $0.tripId } ))
-			
-			let trips = findTrips(with: commonTripIds, on: date, realm: realm)
-			
-			let resultTripIds: [String] = trips.map { $0.id }
-			
-			guard let startingStopTime =  startingStopTimes.filter(using: "tripId IN %@", resultTripIds).first else {
-				Logger.debug("no starting stop time for pairing \(pairing.description)")
-				return nil
-			}
-			
-			guard let destinationStopTime = destinationStopTimes.filter(using: "stopId == %@ AND tripId == %@ AND stopSequence > %@",
-				        destinationStop.id, startingStopTime.tripId, startingStopTime.stopSequence).first else {
-				Logger.debug("no destination stop time found for pairing \(pairing.description)")
-				return nil
-			}
-			
-			guard let trip = trips.filter(using: "id == %@", startingStopTime.tripId).first else {
-				Logger.error("why cant i find a trip what \(pairing.description)")
-				return nil
-			}
-			
-			let summary = TripSummaryImpl()
-			summary._startingStop = startingStopTime
-			summary._destinationStop = destinationStopTime
-			summary._trip = trip
-
-			return summary
-			
-		} catch let error {
-			Logger.error("error with finding trips", error: error)
-		}
-		
-		return nil
+        let date = currentDate
+        let currentTimeInterval = Date().timeIntervalSince(date)
+        if let summary = nextTripSummary(forPairing: pairing, date: date, timeInterval: currentTimeInterval) {
+            return summary
+        }
+        
+        let nextDate = date + (3600 * 24)
+        if let nextDayTripSummary = nextTripSummary(forPairing: pairing, date: nextDate, timeInterval: 0) {
+            return nextDayTripSummary
+        }
+        
+        return nil
 	}
+    
+    private func nextTripSummary(forPairing pairing: StopPairing, date: Date, timeInterval: TimeInterval) -> TripSummary? {
+        guard let startingStop = pairing.startingStop, let destinationStop = pairing.destinationStop else {
+            Logger.error("one of startingStop or destinationStop was nil in pairing")
+            return nil
+        }
+        do {
+            let realm = try Realm()
+            Logger.debug("searching for next trip from \(startingStop.name) to \(destinationStop.name) on \(date)")
+            
+            let startingStopTimes = realm.allObjects(ofType: StopTimeImpl.self)
+                .filter(using: "stopId == %@ AND departureTime >= %@", startingStop.id, timeInterval)
+                .sorted(onProperty: "departureTime")
+            
+            let destinationStopTimes = realm.allObjects(ofType: StopTimeImpl.self)
+                .filter(using: "stopId == %@ AND arrivalTime >= %@", destinationStop.id, timeInterval)
+                .sorted(onProperty: "arrivalTime")
+            
+            
+            let commonTripIds = Set<String>(startingStopTimes.map { $0.tripId })
+                .intersection(Set<String>(destinationStopTimes.map { $0.tripId } ))
+            
+            let trips = findTrips(with: commonTripIds, on: date, realm: realm)
+            
+            let resultTripIds: [String] = trips.map { $0.id }
+            
+            guard let startingStopTime =  startingStopTimes.filter(using: "tripId IN %@", resultTripIds).first else {
+                Logger.debug("no starting stop time for pairing \(pairing.description)")
+                return nil
+            }
+            
+            guard let destinationStopTime = destinationStopTimes.filter(using: "stopId == %@ AND tripId == %@ AND stopSequence > %@",
+                                                                        destinationStop.id, startingStopTime.tripId, startingStopTime.stopSequence).first else {
+                Logger.debug("no destination stop time found for pairing \(pairing.description)")
+                return nil
+            }
+            
+            guard let trip = trips.filter(using: "id == %@", startingStopTime.tripId).first else {
+                Logger.error("why cant i find a trip what \(pairing.description)")
+                return nil
+            }
+            
+            let summary = TripSummaryImpl()
+            summary._startingStop = startingStopTime
+            summary._destinationStop = destinationStopTime
+            summary._trip = trip
+            summary._route = routesById[trip.routeId]
+            
+            return summary
+            
+        } catch let error {
+            Logger.error("error with finding trips", error: error)
+        }
+        
+        return nil
+    }
 	
 	private func findTrips(with tripIds: Set<String>, on date: Date, realm: Realm) -> Results<TripImpl> {
-		Logger.debug("tripIds to query \(tripIds)")
+		Logger.debug("\(tripIds.count) tripIds to query")
 		
 		let calendarDates = realm.allObjects(ofType: CalendarDateImpl.self).filter(using: "date == %@", date)
 		let serviceIds: [String] = calendarDates.map { $0.serviceId }
